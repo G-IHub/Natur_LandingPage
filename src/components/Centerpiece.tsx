80423143a4b006533547c51064a452e9b42775d9
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2026-05-18 14:22:36 +0100

    Hero Section fix.

--- src/components/Centerpiece.tsx
+++ src/components/Centerpiece.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import phoneMockup from "../assets/image2.png";
-import wave from "../assets/wave.svg";
 import doc1 from "../assets/image.svg";
 import doc2 from "../assets/image1.svg";
+
 
 const Centerpiece: React.FC = () => {
   return (
     <div className="relative flex justify-center mt-16 animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.8s] opacity-0 w-full">
       <div className="relative w-full h-125 lg:h-150">
         {/* Phone Mockup */}
-        <div className="absolute left-[70%] lg:left-175 bottom-[-25%] lg:-bottom-40 -translate-x-1/2 w-[350px] lg:w-125 z-10">
+        <div className="absolute left-[70%] lg:left-1/2 bottom-[-25%] lg:-bottom-20 -translate-x-1/2 w-[350px] lg:w-[420px] z-10">
           <img
             src={phoneMockup}
             alt="WhatsApp Chat"
@@ -18,33 +18,28 @@ const Centerpiece: React.FC = () => {
         </div>
 
         {/* Floating Profile Circles */}
-        <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#FED73A] top-[10%] lg:top-0 left-[5%] lg:left-[15%] w-20 h-20 lg:w-30 lg:h-30 animate-[float_5s_ease-in-out_infinite] px-2">
+        <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#FED73A] top-[10%] lg:top-[10%] left-[5%] lg:left-[-10%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] px-2">
           <img src={doc1} alt="Doctor" className="w-full h-auto" />
         </div>
-        <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#897BD6] top-[10%] lg:top-0 right-[5%] lg:right-[20%] w-20 h-20 lg:w-30 lg:h-30 animate-[float_5s_ease-in-out_infinite] [animation-delay:-1s] px-2">
+        <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#897BD6] top-[10%] lg:top-[10%] right-[5%] lg:right-[20%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] [animation-delay:-1s] px-2">
           <img src={doc2} alt="Doctor" className="w-full h-auto" />
         </div>
-        <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#3ADEFE] bottom-[15%] left-[-2%] lg:left-[5%] w-20 h-20 lg:w-30 lg:h-30 animate-[float_5s_ease-in-out_infinite] [animation-delay:-2s] px-2">
+        <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#3ADEFE] bottom-[15%] left-[-2%] lg:left-[-20%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] [animation-delay:-2s] px-2">
           <img src={doc1} alt="Doctor" className="w-full h-auto" />
         </div>
-        <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#482BE6] bottom-[10%] right-0 lg:right-[10%] w-20 h-20 lg:w-30 lg:h-30 animate-[float_5s_ease-in-out_infinite] [animation-delay:-3s] px-2">
+        <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#482BE6] bottom-[10%] right-0 lg:right-[20%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] [animation-delay:-3s] px-2">
           <img src={doc2} alt="Doctor" className="w-full h-auto" />
         </div>
 
         {/* Floating Labels */}
-        <div className="absolute bg-white p-2 lg:p-2.5 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 top-[34%] left-[-2%] lg:top-[20%] lg:left-[30%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-1.5s]">
+        <div className="absolute bg-white p-2 lg:p-2.5 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 top-[34%] left-[-2%] lg:top-[25%] lg:left-[-10%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-1.5s]">
           Analyze your symptoms
         </div>
-        <div className="absolute bg-white p-2 lg:py-2.5 lg:px-10 rounded-l-full rounded-tr-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[32%] right-[4%] lg:bottom-[42%] lg:right-[32%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-0.5s]">
+        <div className="absolute bg-white p-2 lg:py-2.5 lg:px-10 rounded-l-full rounded-tr-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[32%] right-[4%] lg:bottom-[35%] lg:right-[20%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-0.5s]">
           Get Treatment
         </div>
-        <div className="absolute bg-white py-2 px-4 lg:py-2.5 lg:px-10 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[14%] left-[22%] lg:bottom-[20%] lg:left-[32%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-2.5s]">
+        <div className="absolute bg-white py-2 px-4 lg:py-2.5 lg:px-10 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[14%] left-[22%] lg:bottom-[20%] lg:left-[-10%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-2.5s]">
           Feel better
-        </div>
-        <div className="w-full hidden lg:block">
-          <img src={wave} alt="Wave" className="absolute bottom-0" />
-          <img src={wave} alt="Wave" className="absolute bottom-30" />
-          <img src={wave} alt="Wave" className="absolute bottom-60" />
         </div>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,8 @@ import Partnership from "../components/Partnership";
 import Testimonials from "../components/Testimonials";
 import BottomCTA from "../components/BottomCTA";
 import Footer from "../components/Footer";
+import wave from "../assets/wave.svg";
+
 
 const Home: React.FC = () => {
   return (
@@ -16,20 +18,22 @@ const Home: React.FC = () => {
         <div className="relative z-10">
           <Navbar />
 
-          <main className="pt-28 lg:pt-37 bg-[#F8FFF8] text-center px-4 lg:px-0 space-y-4">
-            <div className="max-w-7xl mx-auto">
+          <main className="pt-32 lg:pt-12 pb-16 lg:pb-0 bg-[#F8FFF8] px-4 lg:px-8 w-full overflow-hidden">
+            <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-between gap-12 lg:gap-8">
               {/* Hero Content */}
-              <div className="flex flex-col gap-4 lg:gap-8">
-                <h1 className="text-2xl lg:text-5xl font-bold font-[Arial] max-w-220 mx-auto leading-[1.2] text-black animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.2s] opacity-0">
+              <div className="flex flex-col gap-4 lg:gap-8 lg:w-1/2 text-center lg:text-left z-20">
+                <h1 className="text-2xl lg:text-5xl font-bold font-[Arial] leading-[1.2] text-black animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.2s] opacity-0">
                   <span className="text-primary">24/7</span> Personalized
                   Natural Wellness via WhatsApp, Powered by AI.
                 </h1>
-                <p className="text-xs lg:text-sm font-[Inter] text-[#001621] text-text-muted max-w-175 mx-auto leading-[1.6] animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.4s] opacity-0">
+                <p className="text-xs lg:text-sm font-[Inter] text-[#001621] text-text-muted leading-[1.6] animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.4s] opacity-0 max-w-xl mx-auto lg:mx-0">
                   Discover your custom natural remedy using African medicinal
-                  wisdom and modern diagnostics.  <br className="hidden lg:block" />Overcome generic health
-                  solutions. Access personalized wellness guidance instantly.
+                  wisdom and modern diagnostics.{" "}
+                  <br className="hidden lg:block" />
+                  Overcome generic health solutions. Access personalized
+                  wellness guidance instantly.
                 </p>
-                <div className="flex justify-center font-[Inter] animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.6s] opacity-0">
+                <div className="flex justify-center lg:justify-start font-[Inter] animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.6s] opacity-0">
                   <button className="flex items-center justify-center gap-3 px-4 py-2 lg:px-10 lg:py-4 rounded-full font-semibold text-xs lg:text-[0.7rem] bg-primary text-white shadow-[0_4px_15px_rgba(21,186,36,0.3)] hover:bg-primary-dark hover:-translate-y-0.5 hover:shadow-[0_6px_20px_rgba(21,186,36,0.4)] transition-all duration-300">
                     <svg
                       className="w-5 h-5"
@@ -49,9 +53,17 @@ const Home: React.FC = () => {
                   </button>
                 </div>
               </div>
+
+              {/* Centerpiece */}
+              <div className="flex justify-center items-center lg:w-1/2 w-full z-10">
+                <Centerpiece />
+              </div>
             </div>
-            <div className="-mt-16 flex justify-center items-end w-full">
-              <Centerpiece />
+
+            <div className="w-full hidden lg:block">
+              <img src={wave} alt="Wave" className="absolute bottom-0" />
+              <img src={wave} alt="Wave" className="absolute bottom-30" />
+              <img src={wave} alt="Wave" className="absolute bottom-60" />
             </div>
           </main>
         </div>
@@ -59,10 +71,10 @@ const Home: React.FC = () => {
 
       <div className="relative z-10 bg-white">
         <HowItWorks />
-        <Privacy />
         <Partnership />
         <Testimonials />
         <BottomCTA />
+        <Privacy />
         <Footer />
       </div>
     </div>
