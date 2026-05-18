ef995eefab6de4ad517fe5ec0aaec36e6bbb87bf
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2026-05-18 16:19:00 +0100

    Update2

--- src/components/Centerpiece.tsx
+++ src/components/Centerpiece.tsx
@@ -6,10 +6,10 @@ import doc2 from "../assets/image1.svg";
 
 const Centerpiece: React.FC = () => {
   return (
-    <div className="relative flex justify-center mt-16 animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.8s] opacity-0 w-full">
+    <div className="relative flex justify-center mt-16 animate-[fadeIn_1s_ease-out_forwards] [animation-delay:0.8s] opacity-0 w-full border-2 border-green-100 rounded-full bg-green-50 overflow-hidden">
       <div className="relative w-full h-125 lg:h-150">
         {/* Phone Mockup */}
-        <div className="absolute left-[70%] lg:left-1/2 bottom-[-25%] lg:-bottom-20 -translate-x-1/2 w-[350px] lg:w-[420px] z-10">
+        <div className="absolute left-[70%] lg:left-[60%] bottom-[5%] lg:bottom-0 -translate-x-1/2 w-[250px] lg:w-[300px] z-10">
           <img
             src={phoneMockup}
             alt="WhatsApp Chat"
@@ -18,7 +18,7 @@ const Centerpiece: React.FC = () => {
         </div>
 
         {/* Floating Profile Circles */}
-        <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#FED73A] top-[10%] lg:top-[10%] left-[5%] lg:left-[-10%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] px-2">
+        {/* <div className="absolute rounded-l-full rounded-tr-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#FED73A] top-[10%] lg:top-[10%] left-[5%] lg:left-[-10%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] px-2">
           <img src={doc1} alt="Doctor" className="w-full h-auto" />
         </div>
         <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#897BD6] top-[10%] lg:top-[10%] right-[5%] lg:right-[20%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] [animation-delay:-1s] px-2">
@@ -29,16 +29,16 @@ const Centerpiece: React.FC = () => {
         </div>
         <div className="absolute rounded-r-full rounded-tl-full overflow-hidden flex items-end justify-center shadow-md z-20 transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2.5 hover:z-20 bg-[#482BE6] bottom-[10%] right-0 lg:right-[20%] w-20 h-20 lg:w-28 lg:h-28 animate-[float_5s_ease-in-out_infinite] [animation-delay:-3s] px-2">
           <img src={doc2} alt="Doctor" className="w-full h-auto" />
-        </div>
+        </div> */}
 
         {/* Floating Labels */}
-        <div className="absolute bg-white p-2 lg:p-2.5 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 top-[34%] left-[-2%] lg:top-[25%] lg:left-[-10%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-1.5s]">
+        <div className="absolute bg-white py-1 px-2 lg:py-1 lg:px-4 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-xs shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 top-[34%] left-[5%] lg:top-[35%] lg:left-[10%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-1.5s]">
           Analyze your symptoms
         </div>
-        <div className="absolute bg-white p-2 lg:py-2.5 lg:px-10 rounded-l-full rounded-tr-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[32%] right-[4%] lg:bottom-[35%] lg:right-[20%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-0.5s]">
+        <div className="absolute bg-white py-1 px-2 lg:py-1 lg:px-4 rounded-l-full rounded-tr-full font-[Inter] font-medium text-[#505050] text-xs lg:text-xs shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[45%] right-[5%] lg:bottom-[35%] lg:right-[18%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-0.5s]">
           Get Treatment
         </div>
-        <div className="absolute bg-white py-2 px-4 lg:py-2.5 lg:px-10 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-sm shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[14%] left-[22%] lg:bottom-[20%] lg:left-[-10%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-2.5s]">
+        <div className="absolute bg-white py-1 px-2 lg:py-1 lg:px-4 rounded-r-full rounded-tl-full font-[Inter] font-medium text-[#505050] text-xs lg:text-xs shadow-xl z-20 whitespace-nowrap border border-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/90 bottom-[33%] left-[25%] lg:bottom-[30%] lg:left-[20%] animate-[float_5s_ease-in-out_infinite] [animation-delay:-2.5s]">
           Feel better
         </div>
       </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full py-8 text-center text-text-muted border-t border-gray-100">
+    <footer className="w-full py-8 text-center text-text-muted text-black border-t bg-green-100 border-gray-100">
       <p>&copy; {new Date().getFullYear()} Natur. All rights reserved.</p>
     </footer>
   );
